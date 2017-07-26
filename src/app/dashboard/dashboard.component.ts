3ef38120c69baa2f437a934735d3b0a576068c66
somdevmehta <somu5623@gmail.com>
2017-07-26 18:30:25 +0530

    Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {MdCard} from '@angular/material'
+import { MdCard } from '@angular/material'
+import { Project } from '../classes/project';
+import { Client } from '../classes/client';
+import { ProjectService } from '../services/project.service';
+import { ClientService } from '../services/client.service';
+import { UserService } from '../services/user.service';
+import { Department } from '../classes/department';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,13 +14,25 @@ import {MdCard} from '@angular/material'
 })
 export class DashboardComponent implements OnInit {
 
-  currentEmployee:any;
-  gender:["Male","Female"]
+  employee: any;
+  user: any;
+  department: string
+  projectCount: number;
+  gender = ['Male', 'Female'];
+  clientCount: number;
+  year:number;
 
-  constructor() { }
+  constructor(private projectService: ProjectService, private clientService: ClientService, private userService: UserService) { }
 
   ngOnInit() {
-    this.currentEmployee=JSON.parse(sessionStorage.getItem("currentEmployee"));
+    let user = JSON.parse(sessionStorage.getItem("currentUser"));
+    let userId = user.eid
+    this.userService.getDepartmentById(userId).subscribe(dept => this.department = dept);
+    this.userService.getEmployeeById(userId).subscribe(employee => {this.employee = employee;console.log(employee)});
+    this.userService.getProjectsById(userId).subscribe(projects => this.projectCount = projects._embedded.projects.length);
+    this.userService.getClientsById(userId).subscribe(clients => this.clientCount = clients._embedded.clients.length);
+    let date:Date=new Date()
+    this.year=+date.getFullYear() 
   }
 
 }
