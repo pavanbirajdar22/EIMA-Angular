8d8176225804f72e49860f0a5584c33a275bd0e2
somdevmehta <somu5623@gmail.com>
2017-07-27 20:47:07 +0530

    forms

--- src/app/custom-forms/edit-permission-by-admin/edit-permission-by-admin.component.ts
+++ src/app/custom-forms/edit-permission-by-admin/edit-permission-by-admin.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { MdRadioGroup, MdRadioButton } from '@angular/material';
+import { MdRadioGroup, MdRadioButton,MdSnackBar } from '@angular/material';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Http } from '@angular/http';
 import { EditPermissionByAdminService } from './edit-permission-by-admin.service';
 import { Permission } from './permission';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 @Component({
   selector: 'app-edit-permission-by-admin',
@@ -17,11 +17,13 @@ export class EditPermissionByAdminComponent implements OnInit {
   eid: any;
   permission: Permission;
 
-  constructor(private fb: FormBuilder, private service: EditPermissionByAdminService, private ar: ActivatedRoute, private userService: UserService) {
+  constructor(private fb: FormBuilder, private service: EditPermissionByAdminService, private ar: ActivatedRoute, private userService: UserService,private router:Router,private snackBar:MdSnackBar) {
 
     this.ar.data.subscribe(data => {
       this.permission = data['permission']
       console.log("employee received: " + JSON.stringify(this.permission))
+      this.eid=this.permission.eid
+
 
       this.permissionsForm = this.fb.group({
         addEmployee: [this.permission.addEmployee],
@@ -45,10 +47,25 @@ export class EditPermissionByAdminComponent implements OnInit {
 
   onSubmit() {
     this.permission = this.permissionsForm.value
+    this.permission.eid=this.eid
+    console.log(this.permission)
+    let user =JSON.parse(sessionStorage.getItem('currentUser'))
+    user.permission=this.permission
+    //sessionStorage.clear()
+    sessionStorage.setItem('currentUser',JSON.stringify(user)) 
     this.service.editPermissionByAdmin(this.permission).subscribe(ele => {
       console.log(ele)
-
+      if(ele){
+        this.openSnackBar("Permissions edited sucessfully", " ")
+        this.router.navigate(['/dashboard'])
+      }
     })
+  }
+
+    openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 
 }
